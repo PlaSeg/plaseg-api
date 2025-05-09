9d96766b49e62d2f61c4542a4237cdb90aff843f
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-05-09 14:22:49 -0300

    fix: remove getOpportunityId import. #26

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -20,7 +20,6 @@ import { createManagement } from "./controllers/municipality/create-management.c
 import { createMaintenanceContract } from "./controllers/municipality/create-maintenance-contract.controller";
 
 import { opportunitiesRoutes } from "./controllers/opportunities/opportunities.routes";
-import { getOpportunityById } from "./controllers/opportunities/get-opportunity-by-id.controller";
 import { productsRoutes } from "./controllers/products/products.routes";
 
 const version = "1.0.0 - Release 1";
@@ -59,13 +58,13 @@ export function buildApp(app = fastify().withTypeProvider<ZodTypeProvider>()) {
 	app.register(authRoutes);
 	app.register(opportunitiesRoutes);
 	app.register(productsRoutes);
+
 	app.register(createMunicipality);
 	app.register(createQualifiedStaff);
 	app.register(createProjectPartnership);
 	app.register(createAllocationDepartment);
 	app.register(createManagement);
 	app.register(createMaintenanceContract);
-
 
 	return app;
 }
