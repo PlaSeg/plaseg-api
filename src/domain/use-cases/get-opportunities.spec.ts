8d5a3e1c5d66bf98b8da07dd8df58e5faa429840
thalesapenas <thales.rodrigues@ufpi.edu.br>
2025-05-11 17:22:08 -0300

    refactor: get oportunidade agora retorna a descrição do tipo ao invés do typeId. closes #51

--- src/domain/use-cases/get-opportunities.spec.ts
+++ src/domain/use-cases/get-opportunities.spec.ts
@@ -2,18 +2,33 @@ import { describe, it, beforeEach } from "vitest";
 import { InMemoryOpportunitiesRepository } from "../../../test/repositories/in-memory-opportunities-repository";
 import { GetOpportunitiesUseCase } from "./get-opportunities";
 import { makeOpportunity } from "../../../test/factories/make-opportunity";
+import { InMemoryTypesRepository } from "../../../test/repositories/in-memory-types-repository";
+import { makeType } from "../../../test/factories/make-type";
+import { TypeGroup } from "../../domain/entities/value-objects/type-group";
 
 let inMemoryOpportunitiesRepository: InMemoryOpportunitiesRepository;
+let inMemoryTypesRepository: InMemoryTypesRepository;
 let sut: GetOpportunitiesUseCase;
 
 describe("Get Opportunities Use Case", () => {
 	beforeEach(() => {
 		inMemoryOpportunitiesRepository = new InMemoryOpportunitiesRepository();
-		sut = new GetOpportunitiesUseCase(inMemoryOpportunitiesRepository);
+		inMemoryTypesRepository = new InMemoryTypesRepository();
+		sut = new GetOpportunitiesUseCase(
+			inMemoryOpportunitiesRepository,
+			inMemoryTypesRepository
+		);
 	});
 
 	it("should be able to get opportunities", async () => {
-		const opportunity = makeOpportunity();
+		const type = makeType({
+			group: TypeGroup.opportunity(),
+		});
+		await inMemoryTypesRepository.create(type);
+
+		const opportunity = makeOpportunity({
+			typeId: type.id.toString(),
+		});
 
 		await inMemoryOpportunitiesRepository.create(opportunity);
 
@@ -27,6 +42,7 @@ describe("Get Opportunities Use Case", () => {
 			expect(result.value.opportunities).toEqual([
 				expect.objectContaining({
 					id: opportunity.id.toString(),
+					typeDescription: type.description,
 				}),
 			]);
 		}
--- src/infra/database/prisma/use-cases/make-get-opportunity-by-id-use-case.ts
+++ src/infra/database/prisma/use-cases/make-get-opportunity-by-id-use-case.ts
@@ -1,10 +1,15 @@
 import { PrismaOpportunitiesRepository } from "../repositories/prisma-opportunities-repository";
 import { GetOpportunityByIdUseCase } from "../../../../domain/use-cases/get-opportunity-by-id";
+import { PrismaTypeRepository } from "../repositories/prisma-type-repository";
 
 export function makeGetOpportunityByIdUseCase() {
 	const opportunitiesRepository = new PrismaOpportunitiesRepository();
+	const typesRepository = new PrismaTypeRepository();
 
-	const useCase = new GetOpportunityByIdUseCase(opportunitiesRepository);
+	const useCase = new GetOpportunityByIdUseCase(
+		opportunitiesRepository,
+		typesRepository
+	);
 
 	return useCase;
 }
