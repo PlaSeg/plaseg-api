bad61dce90394cf2fcbf02b7c20877900aa8b2d1
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-05-15 14:19:33 -0300

    feat: remoção de alguns schemas depreciados. #64

--- src/infra/http/schemas/allocation-department.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const createAllocationDepartmentBodySchema = z.object({
-	description: z.string().min(1, "Descrição é obrigatória"),
-	address: z.string().min(1, "Endereço é obrigatório"),
-});
