26d87a52e86f8fefb067b733e55ce90dc040fae2
Jailson Zarur <jailson.zarur@salesrun.io>
2025-05-30 17:01:20 -0300

    feat: remoção do antigo Tipo de Projeto. #109

--- src/domain/repositories/project-type-repository.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectType } from "../entities/project-type";
-
-export interface ProjectTypesRepository {
-	create(projectType: ProjectType): Promise<void>;
-	findById(id: string): Promise<ProjectType | null>;
-	findByName(name: string): Promise<ProjectType | null>;
-	findMany(): Promise<ProjectType[]>;
-}
--- src/infra/database/prisma/use-cases/make-create-project-type-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { CreateProjectTypeUseCase } from "../../../../domain/use-cases/project-types/create-project-type";
-import { PrismaProjectTypesRepository } from "../repositories/prisma-project-type-repository";
-
-export function makeCreateProjectTypeUseCase() {
-    const ProjectTypesRepository = new PrismaProjectTypesRepository();
-    const useCase = new CreateProjectTypeUseCase(ProjectTypesRepository);
-
-    return useCase;
-}
