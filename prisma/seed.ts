10b45c72ad8c43798fb171dd3f0e716e8a589502
thalesapenas <thales.rodrigues@ufpi.edu.br>
2025-06-01 14:07:46 -0300

    feat: adicionar novo seed para project seguindo as logicas de criação do documentos mesclados e etc. #118

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,14 @@ import { seedUsers } from "./seed/users";
 import { seedTypes } from "./seed/types";
 import { seedOpportunities } from "./seed/opportunities";
 import { seedBaseProducts } from "./seed/base-products";
+import { seedProjects } from "./seed/projects";
 
 const prisma = new PrismaClient();
 
 async function seed() {
 	console.log("🚀 Starting seed process...");
 	console.log("🧹 Cleaning existing data...");
+	await prisma.project.deleteMany();
 	await prisma.opportunity.deleteMany();
 	await prisma.baseProduct.deleteMany();
 	await prisma.type.deleteMany();
@@ -24,6 +26,8 @@ async function seed() {
 	await seedOpportunities(prisma, editalId);
 
 	await seedBaseProducts(prisma, weaponId);
+
+	await seedProjects(prisma, editalId);
 
 	console.log("🎉 Seed process completed successfully!");
 }
