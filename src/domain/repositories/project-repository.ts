9acc14a185b80a1a465da620ccf60c0072dad983
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-06-05 17:34:43 -0300

    feat: mudança no project repository. #126

--- src/domain/repositories/project-repository.ts
+++ src/domain/repositories/project-repository.ts
@@ -4,6 +4,7 @@ export interface ProjectsRepository {
 	findById(id: string): Promise<Project | null>;
 	findMany(): Promise<Project[]>;
 	findByTitle(title: string): Promise<Project[] | null>;
+	findManyByMunicipality(municipalityId: string): Promise<Project[]>;
 	create(
 		project: Project
 	): Promise<void>;
