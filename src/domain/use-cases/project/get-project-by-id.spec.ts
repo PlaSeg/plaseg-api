f34ffb5b67b153b60fe705e387925480202aae30
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-06-13 14:48:42 -0300

    feat: criação do caso de uso e seu teste unitário. #133

---
+++ src/domain/use-cases/project/get-project-by-id.spec.ts
@@ -0,0 +1,40 @@
+import { describe, it, beforeEach, expect } from "vitest";
+import { InMemoryProjectsRepository } from "../../../../test/repositories/in-memory-projects-repository";
+import { GetProjectByIdUseCase } from "./get-project-by-id";
+import { makeProject } from "../../../../test/factories/make-project";
+
+let inMemoryProjectsRepository: InMemoryProjectsRepository;
+let sut: GetProjectByIdUseCase;
+
+describe("Get Project By Id Use Case", () => {
+	beforeEach(() => {
+		inMemoryProjectsRepository = new InMemoryProjectsRepository();
+		sut = new GetProjectByIdUseCase(inMemoryProjectsRepository);
+	});
+
+	it("should be able to get a project by id", async () => {
+		const project = makeProject();
+		await inMemoryProjectsRepository.create(project);
+
+		const result = await sut.execute({
+			projectId: project.id.toString(),
+		});
+
+		expect(result.isRight()).toBe(true);
+		if (result.isRight()) {
+			expect(result.value.project).toEqual(project);
+		}
+	});
+
+	it("should return an error when project does not exist", async () => {
+		const result = await sut.execute({
+			projectId: "non-existent-id",
+		});
+
+		expect(result.isLeft()).toBe(true);
+		if (result.isLeft()) {
+			expect(result.value.statusCode).toBe(400);
+			expect(result.value.message).toBe("Não existe um projeto com esse id.");
+		}
+	});
+});
