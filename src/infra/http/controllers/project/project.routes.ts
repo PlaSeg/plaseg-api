ce17f1e667946e971930b0703d9f095ebb5d12b6
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-06-13 14:49:25 -0300

    feat: registro da nova controller. closes #133

--- src/infra/http/controllers/project/project.routes.ts
+++ src/infra/http/controllers/project/project.routes.ts
@@ -4,6 +4,7 @@ import { patchProjectGeneralInfo } from "./patch-project-general-info.controller
 import { createProjectRequestedItem } from "./create-project-requested-item.controller";
 import { patchProjectDocumentField } from "./patch-project-document-field.controller";
 import { getProjects } from "./get-projects.controller";
+import { getProjectById } from "./get-project-by-id.controller";
 
 export async function projectsRoutes(app: FastifyInstance) {
 	app.register(createProjectPartially);
@@ -11,4 +12,5 @@ export async function projectsRoutes(app: FastifyInstance) {
 	app.register(createProjectRequestedItem);
 	app.register(patchProjectDocumentField);
 	app.register(getProjects);
+	app.register(getProjectById);
 }
