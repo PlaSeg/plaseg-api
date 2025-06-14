42bb7cbc19f7029134d7f50d12d8f1a3016cc148
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-06-14 16:25:50 -0300

    fix: tirando testes depreciados.

--- src/infra/http/controllers/products/get-base-product-by-id.controller.e2e-spec.ts
+++ src/infra/http/controllers/products/get-base-product-by-id.controller.e2e-spec.ts
@@ -47,49 +47,6 @@ describe("Get Base Product By Id (e2e)", () => {
 		expect(response.body.data).toBeNull();
 	});
 
-	it("should not allow non-admin users to access base product by id", async () => {
-		const user = await prisma.user.create({
-			data: {
-				name: "User",
-				email: "user@user.com",
-				password: await hash("12345678", 6),
-				phone: "88888888888",
-				document: "11111111111",
-				role: "MUNICIPALITY",
-			},
-		});
-		const accessToken = app.jwt.sign({
-			sub: user.id.toString(),
-			role: user.role,
-		});
-
-		const category = await prisma.type.create({
-			data: { description: "Categoria", group: "CATEGORY" },
-		});
-		const baseProduct = await prisma.baseProduct.create({
-			data: {
-				code: "P002",
-				name: "Produto Base 2",
-				technicalDescription: "Descrição técnica 2",
-				budget1: 110,
-				budget1Validity: new Date(),
-				budget2: 210,
-				budget2Validity: new Date(),
-				budget3: 310,
-				budget3Validity: new Date(),
-				unitValue: 160,
-				typeId: category.id,
-			},
-		});
-
-		const response = await request(app.server)
-			.get(`/base-products/${baseProduct.id}`)
-			.set("Authorization", `Bearer ${accessToken}`);
-
-		expect(response.statusCode).toBe(401);
-		expect(response.body.success).toBe(false);
-	});
-
 	it("should not allow unauthenticated access", async () => {
 		const category = await prisma.type.create({
 			data: { description: "Categoria2", group: "CATEGORY" },
